e7611e2f48f9275853b9beed490479221c226c93
Nishant Rai <Nishant Rai>
2023-09-30 22:59:17 +0530

    redux skill added

--- src/skillsData.js
+++ src/skillsData.js
@@ -4,7 +4,7 @@ import html from "./assets/logo_html 1.png"
 import javascript from "./assets/icons8-javascript-240.png"
 import react from "./assets/React-icon.svg.png"
 import git from "./assets/Git-Icon-1788C.png"
-import redux from "./assets/reduxIcon.png"
+import redux from "./assets/redux.svg"
 
 export const skillImage = [
     css,html,javascript, react, git, redux
